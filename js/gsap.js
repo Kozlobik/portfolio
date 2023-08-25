9b0ce5ef22bd51836bbc9511fd636aefc8ba2baa
Denis Kozlov <kozlobik007@mail.ru>
2023-08-25 14:51:56 +0200

    feat: started working with green sock

---
+++ js/gsap.js
@@ -0,0 +1,9 @@
+  const tlStart = gsap.timeline({})
+  
+  tlStart.from('.info',{
+    x:-400,
+    duration:2,
+    opacity:0,
+    ease:"back.out(1.5)",
+    
+  })
--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
+const fadeBlockHeader = document.querySelector('.fade-block-header')
+const fadeBlockSkills = document.querySelector('.fade-block-skills')
+
 if(window.innerWidth <= 631){
 
 // Находим элементы в константы.
 
     const burger = document.querySelector('.burger')
-    const fadeBlock = document.querySelector('.fade-block')
     const mobileMenu = document.querySelector('.header__mobile-menu-list')
     const menuLink = document.querySelectorAll('.menu-link')
     const burgerLines = document.querySelectorAll('.burger__line')
-
-// Вешаем прослушку элемента на бургер.
-
-burger.addEventListener('click',function () {
-console.log('rrrrufjrthegdgjdgjsgj');
-    mobileMenu.classList.toggle('active')
-
-
+    
+    // Вешаем прослушку элемента на бургер.
+    
+    burger.addEventListener('click',function () {
+        mobileMenu.classList.toggle('active')
+        fadeBlockHeader.classList.toggle('active')
+    
     burgerLines.forEach((item)=>{
         item.classList.toggle('active')
     })
-
-    fadeBlock.classList.toggle('active')
+    
 })
 
 // Прослушиваем ссылки мобильного меню, при клике на ссылку закрываетсяя меню и сайт скролится на определённую секцию.
@@ -28,11 +28,29 @@ menuLink.forEach(function(item){
     item.addEventListener('click', function(){
         burger.classList.remove('active')
         mobileMenu.classList.remove('active')
-        fadeBlock.classList.remove('active')
         burgerLines.forEach((item)=>{
             item.classList.toggle('active')
         })
+        fadeBlockHeader.classList.toggle('active')
     })
 })
 
 }
+
+const cardBtn = document.querySelectorAll('.card-btn')
+const cross = document.querySelectorAll('.skill-card-cross')
+
+cross.forEach(function(item){
+    item.addEventListener('click', function(event){
+        event.target.closest('.skill-card').classList.remove('active')
+        fadeBlockSkills.classList.remove('active')
+    })
+})
+
+cardBtn.forEach(function(item){
+    item.addEventListener('click', function(event){
+event.target.closest('.skill-card').classList.add('active')
+fadeBlockSkills.classList.add('active')
+    })
+})
+
