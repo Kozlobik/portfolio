d2ee64e4a92f7fc317ccae32a7a0e8f52cf4912b
Denis Kozlov <kozlobik007@mail.ru>
2023-08-28 14:45:44 +0200

    feat finished doing animation of our skill cards!

--- js/gsap.js
+++ js/gsap.js
@@ -1,4 +1,6 @@
-  const tlStart = gsap.timeline({})
+gsap.registerPlugin(ScrollTrigger);
+
+const tlStart = gsap.timeline({})
   
   tlStart.from('.info',{
     x:-400,
@@ -6,4 +8,18 @@
     opacity:0,
     ease:"back.out(1.5)",
     
+  })
+
+  gsap.from('.skill-card',{
+    stagger:0.3,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        markers:true,
+        trigger: '.my-skills',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 2,
+        toggleActions: 'play none none reverse',
+    }
   })
